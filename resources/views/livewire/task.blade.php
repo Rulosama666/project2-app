2cefd4450783cfb402d7309c67914f12b12a4082
Rulosama666 <rulosama69@gmail.com>
2023-10-09 14:41:25 -0600

    asds

--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -2,6 +2,7 @@
     <form class="p-4" wire:submit.prevent="save">
         <div class="mb-4">
             <input wire:model="task.text" class="p-2 bg-gray-200 w-full" type="text" placeholder="Tarea...">
+            @error("task.text")<div class="mt-1 text-red-600 text-sm">{{ $message }}</div>@enderror
         </div>
         <button type="submit" class="bg-indigo-700 text-white font-bold w-full rounded shadow p-2">Guardar</button>
     </form>
